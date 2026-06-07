f5b5e202b90522167940b2a3fd01659e09bf04c8
clutchald <legitclutch94@gmail.com>
2026-06-07 17:44:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from DA_Koyeb.health import emit_positive_health
 API_ID = int(os.environ.get("API_ID", "20342933"))
 API_HASH = os.environ.get("API_HASH", "9233e5deebe6abfc9ba297a9678851be")
 MONGO_URI = os.environ.get("MONGO_URI", "mongodb+srv://RAJ:RAJ@outlook.itqomxm.mongodb.net/?appName=outlook")
-SESSION_NAME = os.environ.get("SESSION_NAME", "BQFRwgAArMER9MTPuJo66dzdLGYNjxBk2OC-qrPpEgHcnSE7XazoBzwN2PCeXJALh-td4hMvMjrcyhLPbniNkaMTicj5z3NzoCl-1ocTG2aLKw7mzqHVo_gsIReSyD-SW3gdnjIY8VLIULISdz13RsdICSFZaYwvjWKOTQKTEg9b-d40n4qTLtEgi5cfSh3YPZW3rLBxMGr4MGE2yCbhgiUs8XS6Nz0rFcukb7wRfZ4OVj15hPPo5nePbiwXpkyqqudfA0t4abQgtn7_mDX-jm3JAxGA9Rxune-kcwjuUXrV_jWNh1IRFKP94LbPtKiBj7E5ikbOxj8Lf7qt6gs3bLOKO21ihAAAAAH1u3FYAA")
+SESSION_STRING = os.environ.get("SESSION_STRING", "BQFRwgAArMER9MTPuJo66dzdLGYNjxBk2OC-qrPpEgHcnSE7XazoBzwN2PCeXJALh-td4hMvMjrcyhLPbniNkaMTicj5z3NzoCl-1ocTG2aLKw7mzqHVo_gsIReSyD-SW3gdnjIY8VLIULISdz13RsdICSFZaYwvjWKOTQKTEg9b-d40n4qTLtEgi5cfSh3YPZW3rLBxMGr4MGE2yCbhgiUs8XS6Nz0rFcukb7wRfZ4OVj15hPPo5nePbiwXpkyqqudfA0t4abQgtn7_mDX-jm3JAxGA9Rxune-kcwjuUXrV_jWNh1IRFKP94LbPtKiBj7E5ikbOxj8Lf7qt6gs3bLOKO21ihAAAAAH1u3FYAA")
 
 mongo_client = AsyncIOMotorClient(MONGO_URI)
 db = mongo_client["RojUserBot"]
@@ -38,7 +38,7 @@ async def delete_worker(client: Client):
             await asyncio.sleep(2)
 
 app = Client(
-    SESSION_NAME, 
+    SESSION_STRING, 
     api_id=int(API_ID) if API_ID else 0, 
     api_hash=API_HASH
 )
